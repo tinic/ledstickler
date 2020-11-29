5dfdeb60e926aee60060d559f5d5f77aaef0a0a4
Tinic Uro <tinicuro@gmail.com>
2020-11-29 21:06:14 +0000

    Use Ninja.

--- build_osx.sh
+++ build_osx.sh
@@ -2,6 +2,6 @@
 mkdir -p build
 cd build
 conan install .. --profile ../conan_osx.profile --build=missing
-cmake .. -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_C_COMPILER=gcc-10
+cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_C_COMPILER=gcc-10
 make -j
 
