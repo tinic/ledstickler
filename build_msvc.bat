c3bb22f8569822cfce729b9672296ba480d8afda
Tinic Uro <tinicuro@gmail.com>
2020-12-02 12:12:20 -0800

    Use call.

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 mkdir -p build
 cd build
 conan install .. --build=missing
