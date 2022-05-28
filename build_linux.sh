048245f8908146e79acf8d45b0bff6f543cb4794
Tinic URo <tinicuro@gmail.com>
2022-05-28 00:11:11 +0000

    Upgrade to gcc11

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 conan config install https://github.com/tinic/conan-profiles.git
 mkdir -p build
 cd build
-conan install .. --profile linux_gcc10_x64 --build=missing
+conan install .. --profile linux_gcc11_x64 --build=missing
 cmake -G "Ninja" ..
 cmake --build .
