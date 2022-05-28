49a7c61952c5089012e06c4cf44ee18b13335060
Tinic URo <tinicuro@gmail.com>
2022-05-28 00:19:27 +0000

    Upgrade

--- build_osx.sh
+++ build_osx.sh
@@ -2,6 +2,6 @@
 conan config install https://github.com/tinic/conan-profiles.git
 mkdir -p build
 cd build
-conan install .. --profile osx_gcc10_x64 --build=missing
+conan install .. --profile osx_gcc11_x64 --build=missing
 cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_C_COMPILER=gcc-11
 cmake --build .
--- build_win.bat
+++ build_win.bat
@@ -1,6 +1,6 @@
 conan config install https://github.com/tinic/conan-profiles.git
 mkdir -p build
 cd build
-conan install .. --profile msys2_mingw_gcc10_x64 --build=missing
+conan install .. --profile msys2_mingw_gcc11_x64 --build=missing
 cmake -G "Ninja" ..
 cmake --build .
