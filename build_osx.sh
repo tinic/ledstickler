5c131fa9efb7a3af13fdecdc8a52280b135d4531
Tinic URo <tinicuro@gmail.com>
2022-05-28 00:15:52 +0000

    Upgrade to gcc11

--- build_osx.sh
+++ build_osx.sh
@@ -3,5 +3,5 @@ conan config install https://github.com/tinic/conan-profiles.git
 mkdir -p build
 cd build
 conan install .. --profile osx_gcc10_x64 --build=missing
-cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_C_COMPILER=gcc-10
+cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_C_COMPILER=gcc-11
 cmake --build .
