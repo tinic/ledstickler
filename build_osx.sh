0a783aa296210de27c673c60185d1cb8d688c262
tinic <tinicuro@gmail.com>
2022-12-20 03:48:05 -0800

    Update build_osx.sh

--- build_osx.sh
+++ build_osx.sh
@@ -3,5 +3,5 @@ conan config install https://github.com/tinic/conan-profiles.git
 mkdir -p build
 cd build
 conan install .. --profile osx_gcc11_x64 --build=missing
-cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_C_COMPILER=gcc-11
+cmake -G "Ninja" .. -DCMAKE_CXX_COMPILER=g++-12 -DCMAKE_C_COMPILER=gcc-12
 cmake --build .
